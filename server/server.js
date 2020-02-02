6003317c0f6d4c9d5c7d9e7109beb6171bc6a51c
Abhishek Bansal <discover.ab@gmail.com>
2020-02-02 20:07:51 +0530

    + add some more middlewares and environment vars

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const app = express()
@@ -11,12 +12,21 @@ const authRoutes = require('./routes/auth')
 
 // app middlewares
 app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cors()) // allows all origins
+
+// OPTIONAL if you want to configure course
+//          this is how its done, below restrict API calls
+//          from localhost:3000 only
+// if(process.env.NODE_ENV == 'development') {
+//     app.use(cors({origin: 'http://localhost:3000'}))
+// }
 
 // middlewares
 // anything comes to /api will be forwarded to authRoutes
 app.use('/api', authRoutes)
 
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000
 app.listen(port, () => {
     console.log(`API is running on port ${port}`)
 })
