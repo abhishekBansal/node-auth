00931eb7c329e535b6f5ff8ebf2879cbe1549a5f
Abhishek Bansal <discover.ab@gmail.com>
2020-02-06 19:38:58 +0530

    + send grid integration, user signup with email confirmation

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 //import controller 
-const {signup} = require('../controllers/auth')
+const {signup, accountActivation} = require('../controllers/auth')
 // validations
 const {signupChecks} = require('../validators/auth')
 const {runValidation} = require('../validators')
 // validate and route
 router.post('/signup', signupChecks, runValidation, signup)
+router.post('/account-activation', accountActivation)
 
 
 module.exports = router
